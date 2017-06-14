dd02135d7ae8911638b3cbdb5bb3a733d1230d2d
Curtis Frazier <fraziercurtis2003@yahoo.com>
2017-06-14 16:27:25 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+fetch ("https://randomuser.me/")
+  then(
+    function(response) {
+       if (response.status !== 200) {
+         console.log(response.status);
+         return;
+       }
+       response.json().then(function(data) {
+       console.log(data);
+
+// 1. Fetch your users data
+// 2. Loop over the data
+// 3. Display it in the `.customers` element
+(function () {
+
+  'use strict';
+
+  // Your Code Goes Here
+for (let i = 0; i <12, i++) {
+  fetch("https://randomuser.me/")
+  .then(
+    
+  )
+}
+
+
+})();
